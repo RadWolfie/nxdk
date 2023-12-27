ecb1a1faeecf6ad4ce3a8419a5746f565112c085
RadWolfie <RadWolfie@users.noreply.github.com>
2023-12-27 13:20:26 -0600

    debug

--- share/toolchain-nxdk.cmake
+++ share/toolchain-nxdk.cmake
@@ -26,6 +26,7 @@ set(WIN32 1)
 set(NXDK 1)
 
 set(CMAKE_C_COMPILER "${NXDK_DIR}/bin/${TOOLCHAIN_PREFIX}-cc")
+# TODO: Try see if can use CMAKE_<lang>_IMPLICIT_LINK_DIRECTORIES will work for simple #pragma comment(lib,"libnxdk_net.lib" in header/source file.
 set(CMAKE_C_STANDARD_LIBRARIES "${NXDK_DIR}/lib/libwinapi.lib ${NXDK_DIR}/lib/xboxkrnl/libxboxkrnl.lib ${NXDK_DIR}/lib/libxboxrt.lib  ${NXDK_DIR}/lib/libpdclib.lib ${NXDK_DIR}/lib/libnxdk_hal.lib ${NXDK_DIR}/lib/libnxdk.lib ${NXDK_DIR}/lib/nxdk_usb.lib ${NXDK_DIR}/lib/libnxdk_net.lib") #"${CMAKE_CXX_STANDARD_LIBRARIES_INIT}"
 set(CMAKE_C_LINK_EXECUTABLE "${NXDK_DIR}/bin/${TOOLCHAIN_PREFIX}-link <FLAGS> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -out:<TARGET> <LINK_LIBRARIES>")
 # Keep in sync with include paths in bin/nxdk-cc
